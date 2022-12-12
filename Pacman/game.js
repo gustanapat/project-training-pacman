430c828005b5ea48716f4c2d3aad19d93a758d77
gustanapat <gusmanyoza123@gmail.com>
2022-12-12 20:49:24 +0700

    Add-pacman

--- Pacman/game.js
+++ Pacman/game.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
-const pacmanFrames = document.getElementById("animation");
+const pacmanFrames = document.getElementById("animations");
 const ghostFrames = document.getElementById("ghosts");
 
 let createRect = (x, y, width, height, color) => {
@@ -11,9 +11,17 @@ let createRect = (x, y, width, height, color) => {
 let fps = 30;
 let oneBlockSize = 20;
 let wallColor = "#342DCA";
-let wallSpaceWidth = oneBlockSize / 1.3;
+let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
+let foodColor = "#FEB897";
+let score = 0;
+
+const DIRECTION_RIGHT = 4
+const DIRECTION_UP = 3
+const DIRECTION_LEFT = 2
+const DIRECTION_BOTTOM = 1
+
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -26,7 +34,7 @@ let map = [
     [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
@@ -48,11 +56,43 @@ let gameLoop = () => {
 
 
 let update = () => {
-    //todo
+    pacman.moveProcess()
+    pacman.eat();
+
 };
+
+let drawFoods = () => {
+    for(let i = 0; i < map.length; i++) {
+        for(let j = 0; j < map[0].length; j++) {
+            if(map[i][j] == 2) {
+                createRect(   
+                    j * oneBlockSize + oneBlockSize / 3,
+                    i * oneBlockSize + oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    foodColor
+                );
+            }
+        }
+    }
+}
+
+let drawScore = () => {
+    canvasContext.font = "30px Emulogic";
+    canvasContext.fillStyle = "white";
+    canvasContext.fillText(
+        "Score: " + score, 
+        0, 
+        oneBlockSize * (map.length + 1) 
+    );
+}
+
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, "black" );
     drawWall();
+    drawFoods();
+    pacman.draw();
+    drawScore();
 };
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
@@ -109,3 +149,38 @@ let drawWall = () => {
     }
   }
 };
+
+let createPacman = () => {
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5
+    );
+};
+
+let createGhost = () => {
+    
+}
+
+createPacman();
+gameLoop();
+
+
+window.addEventListener("keydown", (event) => {
+    let k = event.keyCode
+
+    setTimeout(() => {
+        if(k == 37 || k == 65) {
+            pacman.nextDirection = DIRECTION_LEFT;
+        }else if(k == 38 || k == 87) {
+            pacman.nextDirection = DIRECTION_UP;
+        }else if(k == 39 || k == 68) {
+            pacman.nextDirection = DIRECTION_RIGHT;
+        }else if(k == 40 || k == 83) {
+            pacman.nextDirection = DIRECTION_BOTTOM;
+        }    
+    }, 1);
+});
